d58818adca0f7bba9c69b5e05f60c424fa0e58b0
Lucas Andrade <lucasandradetrabalho@hotmail.com>
2021-06-26 16:52:30 -0300

    first commit

---
+++ Desktop/Coder-Web-JS/controle/for1.js
@@ -0,0 +1,16 @@
+//exemplo 1
+let contador = 1
+while (contador <= 10) {
+    console.log(`contador = ${contador}`)
+    contador++
+}
+// exemplo 1 usando for
+for (let i = 1; i <= 10; i++) {
+    console.log(`contador = ${i}`)
+}
+
+// exemplo 2 usando array
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for (i = 0; i < notas.length; i++) {
+    console.log(`notas = ${notas[i]}`)
+}
